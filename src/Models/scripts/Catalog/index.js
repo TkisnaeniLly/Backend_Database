8f9e55ea6e7bf81273daed49da471357c4699557
wylsonadelwin-lgtm <wylsonadelwin@gmail.com>
2025-12-22 12:06:02 +0700

    Create index.js

---
+++ src/Models/scripts/Catalog/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Brand: require("./Brand"),
+  Category: require("./Category"),
+  Product: require("./Product"),
+  Variant: require("./Variant"),
+  Inventory: require("./Inventory"),
+  Media: require("./Media"),
+};
